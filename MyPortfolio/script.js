723d726dc74c1b69d707ee0158c0afd09b071d2b
teluguabbai <parasu7824@gmail.com>
2024-08-09 09:38:16 +0530

    Update script.js

--- MyPortfolio/script.js
+++ MyPortfolio/script.js
@@ -9,7 +9,7 @@ const navlink = document.querySelectorAll('header nav a')
 window.onscroll = ()=>{
     section.forEach(sec=>{
         let top = window.scrollY;
-        let offset = sec.offsetTop - 10;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id')
         if(top>offset && top < offset + height){
